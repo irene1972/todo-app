19c8a1800ae33cfc7a22e45d9d92f998c2e417fc
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-09-14 11:41:18 +0200

    creación de store

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-import './style.css';
-
-import {App} from './todos/app';
-
 /*
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 */
+import './style.css';
+
+import {App} from './todos/app';
+import todoStore from './store/todo.store';
+
+todoStore.initStore();
+
 App('#app');
 
