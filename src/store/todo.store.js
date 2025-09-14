f37893af3ec4de9f277d3480387f3fca54242bc1
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-09-14 12:38:13 +0200

    add funcionalidades del store

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -25,46 +25,71 @@ const loadStore=()=>{
 }
 /**
  * 
+ * @param {Object} filter 
+ */
+const getTodos=(filter=Filters.All)=>{
+    switch(filter){
+        case Filters.All:
+            return [...state.todos];
+        case Filters.Completed:
+            return state.todos.filter(todo=>todo.done);
+        case Filters.Pending:
+            return state.todos.filter(todo=>!todo.done);
+        default:
+            throw new Error(`Opción ${filter} is not valid.`);
+    }
+}
+
+/**
+ * 
  * @param {String} description 
  */
 const addTodo=(description)=>{
-    throw new Error('Not implemented');
+    if (!description) throw new Error('Description is required');
+    state.todos.push(new Todo(description));
 }
 /**
  * 
  * @param {String} todoId 
  */
 const toggleTodo=(todoId)=>{
-    throw new Error('Not implemented');
+    state.todos=state.todos.map(todo=>{
+    if (todo.id===todoId){
+        todo.done= !todo.done;
+    }
+    return todo;
+    });
 }
 /**
  * 
  * @param {String} todoId 
  */
 const deleteTodo=(todoId)=>{
-    throw new Error('Not implemented');
+    state.todos=state.todos.filter(todo=>todo.id !== todoId);
 }
 
 const deleteCompleted=()=>{
-    throw new Error('Not implemented');
+    state.todos=state.todos.filter(todo=>todo.done);
 }
 
 /**
  * 
- * @param {Object} newFilter 
+ * @param {Filters} newFilter 
  */
 const setFilter=(newFilter=Filters.All)=>{
-    throw new Error('Not implemented');
+    state.filter=newFilter;
 }
 
 const getCurrentFilter=()=>{
-    throw new Error('Not implemented');
+    return state.filter;
 }
 
 export default{
+    addTodo,
     deleteCompleted,
     deleteTodo,
     getCurrentFilter,
+    getTodos,
     initStore,
     loadStore,
     setFilter,
