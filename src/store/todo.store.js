06e94254d2f2c31fcfacaa57bd75387456922ec9
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-09-14 12:05:30 +0200

    preparar los métodos vacíos

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -20,6 +20,53 @@ const initStore=()=>{
     console.log('InitStore');
 }
 
+const loadStore=()=>{
+    throw new Error('Not implemented');
+}
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo=(description)=>{
+    throw new Error('Not implemented');
+}
+/**
+ * 
+ * @param {String} todoId 
+ */
+const toggleTodo=(todoId)=>{
+    throw new Error('Not implemented');
+}
+/**
+ * 
+ * @param {String} todoId 
+ */
+const deleteTodo=(todoId)=>{
+    throw new Error('Not implemented');
+}
+
+const deleteCompleted=()=>{
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {Object} newFilter 
+ */
+const setFilter=(newFilter=Filters.All)=>{
+    throw new Error('Not implemented');
+}
+
+const getCurrentFilter=()=>{
+    throw new Error('Not implemented');
+}
+
 export default{
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo
 }
