818cdef6e4b527addd69de3bf719c2408995d8ff
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-09-14 16:59:07 +0200

    Crear un nuevo todo

--- src/todos/app.js
+++ src/todos/app.js
@@ -3,7 +3,8 @@ import todoStore from '../store/todo.store';
 import { renderTodos } from './use-cases';
 
 const ElementIDs={
-    TodoList:'.todo-list'
+    TodoList:'.todo-list',
+    NewTodoInput:'#new-todo-input'
 }
 
 export const App=(elementId)=>{
@@ -22,4 +23,21 @@ export const App=(elementId)=>{
         document.querySelector(elementId).append(app);
         displayTodos();
     })();
+
+    //Referencias HTML
+    const newDescriptionInput=document.querySelector(ElementIDs.NewTodoInput);
+
+    newDescriptionInput.addEventListener('keyup', (event)=>{
+        //console.log(event);
+        //console.log(event.target.value);
+
+        if(event.keyCode !== 13) return;
+        if(event.target.value.trim().length === 0) return;
+
+        todoStore.addTodo(event.target.value);
+        displayTodos();
+        event.target.value='';
+
+    });
+
 }
